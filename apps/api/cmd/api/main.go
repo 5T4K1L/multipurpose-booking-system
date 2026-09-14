d4c710beeffe3a33ce040c65a22b109e13baa1a2
Alfonzo Akil <akilabiel3@gmail.com>
2026-09-14 12:21:50 +0800

    initialize Go API

---
+++ apps/api/cmd/api/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	log.Println("Server is running on http://localhost:8080")
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Could not listen on %s: %v\n", server.Addr, err)
+	}
+}
---
+++ apps/api/go.mod
@@ -0,0 +1,3 @@
+module multipurpose-booking-system/api
+
+go 1.27.1
